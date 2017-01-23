ec3a89b13783c1e6a7c6fd05b99a4847c59f61d0
artmadiar <artmadiar@gmail.com>
2017-01-23 16:00:50 +0100

    js2xmlparser updated

--- js2xmlparser/index.js
+++ js2xmlparser/index.js
@@ -1,11 +1,21 @@
 const js2xmlparser = require('js2xmlparser');
 
 const parameters =
-  {
-    nombre: 'P_EMPRESA',
-    tipo: {
-        field: 'value'
-      },
-    valor: 'const',
-  };
-console.log(js2xmlparser.parse('parametro', parameters));
+  [
+    {
+      nombre: 'P_EMPRESA',
+      tipo: 'char',
+      valor: 'const',
+    }
+  ];
+
+let sParametros =
+    {
+      pagina: -1,
+      registrospagina: -1,
+      identificadorsql: '',
+      validaciones: 'si',
+      parametros: {parametro: parameters}
+    };
+
+console.log(js2xmlparser.parse('raiz', sParametros));
